55b2e6ebb24dc99b8f3ec0a4a14571cde4b216bd
RonalMinh7 <minhgau25@gmail.com>
2023-12-18 18:02:37 +0700

    New Project

---
+++ Assets/Minh/Scripts/UI/ChooseLevelUI.cs
@@ -0,0 +1,21 @@
+﻿using Obvious.Soap;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Minh
+{
+    public class ChooseLevelUI : MonoBehaviour
+    {
+        [SerializeField] private ScriptableEventNoParam _openMainMenuUI;
+        
+        public void StartGame()
+        {
+            SceneManager.LoadScene("BattleScene");
+        }
+
+        public void ExitToMainMenu()
+        {
+            _openMainMenuUI.Raise();
+        }
+    }
+}
---
+++ Assets/Minh/Scripts/UI/MenuUI.cs
@@ -0,0 +1,22 @@
+﻿using Obvious.Soap;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Minh
+{
+    public class MenuUI : MonoBehaviour
+    {
+        [SerializeField] private ScriptableEventNoParam _eventOpenChooseLevel;
+        public void StartGame()
+        {
+            SceneManager.LoadScene("BattleScene");
+
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+            Debug.Log("Quit");
+        }
+    }
+}
