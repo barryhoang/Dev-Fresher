cb0c9a6290897cdcf5ae474bc24d86d5084c7740
tungrong102102 <onghangxom20k1@gmail.com>
2024-01-09 16:17:27 +0700

    test $1e7

--- Assets/Scripts/Tung/UI/MainMenuManager.cs
+++ Assets/Scripts/Tung/UI/MainMenuManager.cs
@@ -25,6 +25,7 @@ namespace Tung
             _exit.onClick.AddListener(OnClickExit);
             Timing.RunCoroutine(StartGame().CancelWith(gameObject));
             _start.onClick.AddListener(() => Timing.RunCoroutine(OnClickStart().CancelWith(gameObject)));
+            Debug.Log("ABC");
         }
 
         private IEnumerator<float> StartGame()
